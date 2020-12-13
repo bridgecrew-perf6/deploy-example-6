74438258afa1e32aeeb730cd504ddf45a14cd0c4
yamnyk <yamnyk.vlog@gmail.com>
2020-12-13 15:53:06 +0200

    go go go

---
+++ client/src/App.js
@@ -0,0 +1,85 @@
+import React, {useEffect, useState} from "react";
+import './App.css';
+import PrimarySearchAppBar from "./components/AppBar";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/emails', {
+      headers: {
+        "Content-Type": 'application/json'
+      }
+    })
+      .then(r => r.json())
+      .then(data => {
+        setCards(data.inbox || [])
+      })
+  }, []);
+
+  return (
+    <div className="App">
+      <PrimarySearchAppBar/>
+
+      <Container className={classes.cardGrid} maxWidth="md">
+        {/* End hero unit */}
+        <Grid container spacing={4}>
+          {cards.map((card) => (
+            <Grid item key={card.id} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {card.subject}
+                  </Typography>
+                  <Typography>
+                    {card.text}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,148 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const {v4: uuidv4} = require('uuid');
+
+const USERS = [
+  {
+    login: 'admin@g.g',
+    password: '123123',
+  },
+];
+
+const EMAILS = {
+  "inbox": [
+    {
+      "id": "0001",
+      "subject": "Delivery order #32232323",
+      "from": "delivery@eshop.coco.com",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quoolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!"
+    },
+    {
+      "id": "0002",
+      "subject": "You're fired",
+      "from": "boss@bossy.boss.com",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos uehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!"
+    },
+    {
+      "id": "0003",
+      "subject": "Your car is ready!",
+      "from": "legasy@service.com",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequunturt repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic ius dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!"
+    },
+    {
+      "id": "0004",
+      "subject": "Wat do you think about our new features",
+      "from": "body-parser@npm.no",
+      "text": "Lorem ipsum dolor sit amet, consque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!"
+    },
+    {
+      "id": "0005",
+      "subject": "TOP SALE!! 90% OFF",
+      "from": "sales@kexshop.nd",
+      "text": "Lorem ipsum dolor sadipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!"
+    },
+    {
+      "id": "0006",
+      "subject": "Ya tibya po IP vichislil!!!",
+      "from": "unknown",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!"
+    }
+  ],
+  "sent": [
+    {
+      "id": "0007",
+      "subject": "Give me back my money",
+      "to": "sales@kexshop.nd",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!"
+    },
+    {
+      "id": "0008",
+      "subject": "Contract #588820 closed",
+      "to": "big-boss@bossy.boss.com",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur fuga hic iusto nesciunt nostrum porro quos ullam. Cumque dolor eaque eveniet pariatur quam qui repellat repellendus reprehenderit sint ut!"
+    }
+  ],
+  "draft": [
+    {
+      "id": "0009",
+      "subject": "",
+      "to": "hr@terrasoft",
+      "text": "I'm looking for a job...."
+    },
+    {
+      "id": "00010",
+      "subject": "About Tuesday meeting",
+      "to": "nobody@from-the.wc",
+      "text": ""
+    },
+    {
+      "id": "00011",
+      "subject": "",
+      "to": "",
+      "text": "What did yo mean, saying "
+    }
+  ],
+  "spam": []
+};
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.post('/api/users/', (req, res) => {
+  const user = req.body;
+  USERS.push(user);
+  res.send(user);
+});
+
+app.post('/api/users/login', (req, res) => {
+  const user = req.body;
+  const userFromDB = USERS.find(u => u.login === user.login && u.password === user.password);
+  
+  res.send({
+    status: !!userFromDB,
+    data: userFromDB
+  });
+})
+
+app.get("/api/emails", (req, res) => {
+  res.send(EMAILS);
+});
+
+app.get('/api/emails/:folder', (req, res) => {
+  const {folder} = req.params;
+  
+  res.send(EMAILS[folder]);
+})
+
+app.get("/api/emails/:emailID", (req, res) => {
+  const {emailID} = req.params;
+  let requestedEmail = null;
+  
+  Object.keys(EMAILS).forEach(box => {
+    const find = EMAILS[box].find(email => email.id === emailID);
+    
+    if (find) {
+      requestedEmail = find
+    }
+  });
+  res.send(requestedEmail);
+});
+
+app.post("/api/emails/:folder", (req, res) => {
+  const {folder} = req.params;
+  const newEmail = req.body;
+  
+  newEmail.id = uuidv4();
+  
+  EMAILS[folder].push(newEmail);
+  
+  res.send({
+    message: "email saved successfully",
+    data: newEmail
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("server is listening on port " + PORT)
+});
